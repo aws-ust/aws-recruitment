e16a8aa93831f61908d98034af95f68119d9678f
Neil Casas <neilalfonzcasas@gmail.com>
2025-09-13 04:42:03 +0800

    feat: streamlined page.tsx code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { EventsSection } from "@/components/sections/events";
-import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.body}>
+    <>
       <EventsSection />
-    </div>
+    </>
   );
 }
